c75482a94f631e754d5a94bb9d87987d6ae1058f
Sherry <sherry.tung@sincheng.net>
2023-09-02 17:16:49 +0800

    feat: add router.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 // const uploadRouter = require('./router.js')
 
 // Initialize Express
+const dotenv = require("dotenv").config()
 const app = express();
 // app.use(express.json())
 // app.use(express.urlencoded({extended: false}))
@@ -15,11 +16,12 @@ const app = express();
 
 app.get("/", (req, res) => {
     // res.sendFile(__dirname + '/index.html')
+    console.log(process.env.DRIVE_KEY)
       res.send("Express on Vercel test");
 });
 
 // Initialize server
-app.listen(5000, () => {
+app.listen(3030, () => {
   console.log("Running on port 5000.");
 });
 
---
+++ router.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const multer = require("multer");
+// const path = require("path");
+const { google } = require("googleapis");
+const stream = require("stream");
+
+const uploadRouter = express.Router();
+const upload = multer();
+
+// const KEYFILEPATH = path.join(__dirname, "credentials.json");
+
+const SCOPES = ["https://www.googleapis.com/auth/drive"];
+
+const auth = new google.auth.GoogleAuth({
+  //   keyFile: KEYFILEPATH,
+  credentials: {
+    "type": "service_account",
+    "project_id": "zakboxing",
+    "private_key_id": "19bcf1488ba17a5dd639193a57a45f6be6a34738",
+    "private_key": process.env.DRIVE_KEY,
+    "client_email": "ledbackend@zakboxing.iam.gserviceaccount.com",
+    "client_id": "102187801513047450786",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/ledbackend%40zakboxing.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
+  },
+  scopes: SCOPES,
+});
+
+const uploadFile = async (fileObject) => {
+  const bufferStream = new stream.PassThrough();
+  bufferStream.end(fileObject.buffer);
+  const { data } = await google
+    .drive({
+      version: "v3",
+      auth,
+    })
+    .files.create({
+      media: {
+        mimeType: fileObject.mimeType,
+        body: bufferStream,
+      },
+      requestBody: {
+        name: fileObject.originalName,
+        parents: ["1FoJbzJAPFISLePmWqaw3VIiBsJat1GBl"], //GoogleDrive Folder ID
+      },
+      fields: "id, name, thumbnailLink",
+    });
+
+  console.log(`SUCCESSFULLY UPLOADED: ${data.name} ${data.id}`);
+  console.log(data.thumbnailLink);
+};
+
+uploadRouter.post("/upload", upload.any(), async (req, res) => {
+  // console.log(req.body)
+  try {
+    // console.log(req.files)
+    const { body, files } = req;
+    const targetFile = files[0];
+    uploadFile(targetFile);
+    res.status(200).send("SUBMITTED");
+  } catch (err) {
+    res.send(err.message);
+  }
+});
+
+module.exports = uploadRouter;
