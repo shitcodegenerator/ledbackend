322e0f8fc05f57abfd99e43884b6b1467a25fca5
Sherry <sherry.tung@sincheng.net>
2023-09-02 16:44:00 +0800

    text2

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 // Add Express
 const express = require("express");
+// const uploadRouter = require('./router.js')
 
 // Initialize Express
 const app = express();
+// app.use(express.json())
+// app.use(express.urlencoded({extended: false}))
+// app.use(uploadRouter)
 
 // Create GET request
+// app.get("/", (req, res) => {
+//   res.send("Express on Vercel");
+// });
+
 app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+    // res.sendFile(__dirname + '/index.html')
+      res.send("Express on Vercel");
 });
 
 // Initialize server
