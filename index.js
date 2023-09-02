6c65ee030562b716440debf112de05f610ec97c2
Sherry <sherry.tung@sincheng.net>
2023-09-02 18:44:47 +0800

    0644

--- index.js
+++ index.js
@@ -27,25 +27,28 @@ app.use(express.urlencoded({extended: false}))
 //   res.send("Express on Vercel");
 // });
 
-const auth = new google.auth.GoogleAuth({
-    credentials: {
-      "type": "service_account",
-      "project_id": "zakboxing",
-      "private_key_id": "19bcf1488ba17a5dd639193a57a45f6be6a34738",
-      "private_key": process.env.DRIVE_KEY,
-      "client_email": "ledbackend@zakboxing.iam.gserviceaccount.com",
-      "client_id": "102187801513047450786",
-      "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-      "token_uri": "https://oauth2.googleapis.com/token",
-      "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-      "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/ledbackend%40zakboxing.iam.gserviceaccount.com",
-      "universe_domain": "googleapis.com"
-    },
-    scopes: ["https://www.googleapis.com/auth/drive"],
-  });
+
   
   const uploadFile = async (fileObject) => {
-    const bufferStream = new stream.PassThrough();
+    try {
+        const auth = new google.auth.GoogleAuth({
+            credentials: {
+              "type": "service_account",
+              "project_id": "zakboxing",
+              "private_key_id": "19bcf1488ba17a5dd639193a57a45f6be6a34738",
+              "private_key": process.env.DRIVE_KEY,
+              "client_email": "ledbackend@zakboxing.iam.gserviceaccount.com",
+              "client_id": "102187801513047450786",
+              "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+              "token_uri": "https://oauth2.googleapis.com/token",
+              "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+              "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/ledbackend%40zakboxing.iam.gserviceaccount.com",
+              "universe_domain": "googleapis.com"
+            },
+            scopes: ["https://www.googleapis.com/auth/drive"],
+          });
+          
+        const bufferStream = new stream.PassThrough();
     bufferStream.end(fileObject.buffer);
     const { data } = await google
       .drive({
@@ -66,6 +69,9 @@ const auth = new google.auth.GoogleAuth({
   
     console.log(`SUCCESSFULLY UPLOADED: ${data.name} ${data.id}`);
     console.log(data.thumbnailLink);
+    } catch(err) {
+        console.log(err?.message)
+    }
   };
 
 app.post("/upload", upload.any(), async (req, res) => {
@@ -77,7 +83,7 @@ app.post("/upload", upload.any(), async (req, res) => {
       uploadFile(targetFile);
       res.status(200).send("SUBMITTED");
     } catch (err) {
-      res.send(err.message);
+      res.send(err?.message);
     }
   });
 
