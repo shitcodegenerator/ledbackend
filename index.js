a0571bd323e4ccaec05cca47ed228c83e3245d0f
Sherry <sherry.tung@sincheng.net>
2023-09-02 18:48:32 +0800

    fix: 0648

--- index.js
+++ index.js
@@ -21,56 +21,57 @@ app.use(express.urlencoded({ extended: false }));
 //   res.send("Express on Vercel");
 // });
 
-// const uploadFile = async (fileObject) => {
-//   try {
-//     const auth = new google.auth.GoogleAuth({
-//       credentials: {
-//         type: "service_account",
-//         project_id: "zakboxing",
-//         private_key_id: "19bcf1488ba17a5dd639193a57a45f6be6a34738",
-//         private_key: process.env.DRIVE_KEY,
-//         client_email: "ledbackend@zakboxing.iam.gserviceaccount.com",
-//         client_id: "102187801513047450786",
-//         auth_uri: "https://accounts.google.com/o/oauth2/auth",
-//         token_uri: "https://oauth2.googleapis.com/token",
-//         auth_provider_x509_cert_url:
-//           "https://www.googleapis.com/oauth2/v1/certs",
-//         client_x509_cert_url:
-//           "https://www.googleapis.com/robot/v1/metadata/x509/ledbackend%40zakboxing.iam.gserviceaccount.com",
-//         universe_domain: "googleapis.com",
-//       },
-//       scopes: ["https://www.googleapis.com/auth/drive"],
-//     });
 
-//     const bufferStream = new stream.PassThrough();
-//     bufferStream.end(fileObject.buffer);
-//     const { data } = await google
-//       .drive({
-//         version: "v3",
-//         auth,
-//       })
-//       .files.create({
-//         media: {
-//           mimeType: fileObject.mimeType,
-//           body: bufferStream,
-//         },
-//         requestBody: {
-//           name: fileObject.originalName,
-//           parents: ["1FoJbzJAPFISLePmWqaw3VIiBsJat1GBl"], //GoogleDrive Folder ID
-//         },
-//         fields: "id, name, thumbnailLink",
-//       });
-
-//     console.log(`SUCCESSFULLY UPLOADED: ${data.name} ${data.id}`);
-//     console.log(data.thumbnailLink);
-//   } catch (err) {
-//     console.log(err?.message);
-//   }
-// };
 
 app.post("/upload", upload.any(), async (req, res) => {
   // console.log(req.body)
   try {
+    const uploadFile = async (fileObject) => {
+        try {
+          const auth = new google.auth.GoogleAuth({
+            credentials: {
+              type: "service_account",
+              project_id: "zakboxing",
+              private_key_id: "19bcf1488ba17a5dd639193a57a45f6be6a34738",
+              private_key: process.env.DRIVE_KEY,
+              client_email: "ledbackend@zakboxing.iam.gserviceaccount.com",
+              client_id: "102187801513047450786",
+              auth_uri: "https://accounts.google.com/o/oauth2/auth",
+              token_uri: "https://oauth2.googleapis.com/token",
+              auth_provider_x509_cert_url:
+                "https://www.googleapis.com/oauth2/v1/certs",
+              client_x509_cert_url:
+                "https://www.googleapis.com/robot/v1/metadata/x509/ledbackend%40zakboxing.iam.gserviceaccount.com",
+              universe_domain: "googleapis.com",
+            },
+            scopes: ["https://www.googleapis.com/auth/drive"],
+          });
+      
+          const bufferStream = new stream.PassThrough();
+          bufferStream.end(fileObject.buffer);
+          const { data } = await google
+            .drive({
+              version: "v3",
+              auth,
+            })
+            .files.create({
+              media: {
+                mimeType: fileObject.mimeType,
+                body: bufferStream,
+              },
+              requestBody: {
+                name: fileObject.originalName,
+                parents: ["1FoJbzJAPFISLePmWqaw3VIiBsJat1GBl"], //GoogleDrive Folder ID
+              },
+              fields: "id, name, thumbnailLink",
+            });
+      
+          console.log(`SUCCESSFULLY UPLOADED: ${data.name} ${data.id}`);
+          console.log(data.thumbnailLink);
+        } catch (err) {
+          console.log(err?.message);
+        }
+      };
     // console.log(req.files)
     const { body, files } = req;
     // const targetFile = files[0];
