3a5346f4a503678d973f49a3d8ffb2ddee3d03c3
Sherry <sherry.tung@sincheng.net>
2024-03-01 15:35:57 +0800

    f

--- controllers/lotteryMember.js
+++ controllers/lotteryMember.js
@@ -97,7 +97,7 @@ const reset = asyncHandler(async (req, res) => {
     // Update the isWinner field for the winners
     await LotteryMember.updateMany({ _id: { $in: winners.map(user => user._id) } }, { isWinner: false });
 
-    res.status(200).json({ message: '成功', winners });
+    res.status(200).json({ message: '成功' });
   } catch (error) {
     console.error('Error during lottery:', error);
     res.status(500).json({ message: '抽獎失敗', error: 'An error occurred during the lottery.' });
